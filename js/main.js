f2cd19510cd6a0e9fcf974b0cb92aa3b74d5ac31
Quentin Delcourt <kant312@gmail.com>
2015-01-16 01:30:54 +0100

    wip

--- js/main.js
+++ js/main.js
@@ -5,83 +5,90 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	TT.loadDb({
 		//Dialog set
-		"HOME_MAIN_001": {
+		"HOME_MAIN": {
 			//Properties
 			linear: true,
 			type: 'sequence',
 			toDialog: "HOME_MAIN_002",
 			//Lines
-			"lines": [
+			"lines": {
 				//Dialog lines
-				{
-					id: "HOME_001_AVAT",
+				"HOME_001_AVAT": {
 					actor: 'PLAYER',
 					pause: 500,
 					text: "Hey, this is a test..."
 				},
-				{
-					id: "HOME_002_AVAT",
+				"HOME_002_AVAT": {
 					actor: 'PLAYER',
 					pause: 1500,
 					text: "...relax."
 				},
-				{
-					id: "HOME_003_AVAT",
+				"HOME_003_AVAT": {
 					actor: 'PLAYER',
 					pause: 1500,
 					text: "It's no big deal"
 				},
-				{
-					id: "HOME_004_AVAT",
+				"HOME_004_AVAT": {
 					actor: 'HERMAN',
 					pause: 3000,
 					text: "Congrats!"
 				},
-				{
-					id: "HOME_005_AVAT",
+				"HOME_005_HERMAN": {
 					actor: 'HERMAN',
-					pause: 500,
+					pause: 1500,
 					text: "Seems it's working so far..."
 				}
-			]
+			}
 		},
 		//Dialog set
-		"HOME_MAIN_002": {
+		"HOME_CHOICES": {
 			//Properties
 			linear: true,
 			type: 'choice',
 			//Lines
-			"lines": [
+			"lines": {
 				//Dialog lines
-				{
-					id: "HOME_006_AVAT",
+				"HOME_006_AVAT": {
 					actor: 'PLAYER',
 					pause: 0,
 					text: "I could play this all night."
 				},
-				{
-					id: "HOME_007_AVAT",
+				"HOME_007_AVAT": {
 					actor: 'PLAYER',
 					pause: 0,
 					text: "I can't believe how stupid this game is."
 				},
-				{
-					id: "HOME_008_AVAT",
+				"HOME_008_AVAT": {
 					actor: 'PLAYER',
 					pause: 0,
 					text: "Hum."
 				}
-			]
+			}
 		}
 	});
 
+	var actors = {
+		'HERMAN': document.getElementById('HERMAN'),
+		'PLAYER': document.getElementById('PLAYER')
+	};
+
+	var clearDialogs = function() {
+		for(var idx in actors) {
+			actors[idx].innerHTML = '';
+		}
+	}
+
 	TT.on('dialogChoices', function(lines) {
+		var choice = '';
+		clearDialogs();
 		for(var i=0; i<lines.length; i++) {
-			console.log('%c ' + (i+1) + '. ' + lines[i].text, 'background: #fff; color: #da55ba');
+			var line = lines[i];
+			choice = (i+1) + '. ' + line.text + '\n';
+			actors[line.actor].innerHTML += choice;
 		}
 	});
 	TT.on('dialogShown', function(line) {
-		console.log('%c ' + line.text, 'background: #222; color: #bada55');
+		actors[line.actor].innerHTML = line.text;
 	});
 
 	TT.engage('HOME_MAIN_001');
